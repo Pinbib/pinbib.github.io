0eed2f58c749cd81e0d6950a98c97f3bd25804b3
Pinbib <pancenkoe353@gmail.com>
2023-08-25 14:40:30 +0100

    :)

---
+++ main.js
@@ -0,0 +1,42 @@
+function urlJoin(...parts) {
+    const separator = '/';
+    const normalizedParts = parts.map(part => {
+        if (typeof part === 'string') {
+            return part.replace(/\/+$/, '').replace(/^\/+/, '');
+        }
+        return part;
+    });
+    return normalizedParts.join(separator);
+}
+
+function toUrlJoin(path, ...arg) {
+    let url = document.createElement("a");
+    url.href = urlJoin(path, arg);
+    url.target = "_blank";
+    url.click();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    let input = document.getElementById("command");
+    setInterval(() => {
+        if (input.placeholder == "|") {
+            input.placeholder = "";
+        } else {
+            input.placeholder = "|";
+        };
+    }, 500);
+
+    document.addEventListener("keyup", (ev) => {
+        if (ev.key == "Enter") {
+            if (input.value.trim() != "") {
+                let span = document.createElement("span");
+                span.innerText = input.value.trim();
+                document.getElementById("history").append(span);
+
+                toUrlJoin("https://pinbib.github.io", input.value.trim().split(" "));
+                console.log(urlJoin("https://pinbib.github.io", input.value.trim().split(" ")))
+                input.value = "";
+            };
+        };
+    });
+});
