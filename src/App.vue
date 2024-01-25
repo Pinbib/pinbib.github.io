2ede5a49974efd231b0836519985c36f4c0a9a69
Pinbib <pancenkoe353@gmail.com>
2024-01-25 09:58:55 +0000

    added component Subpage

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script>
 import IconTitle from "@/components/IconTitle.vue";
 import Project from "@/components/Project.vue";
+import Subpage from "@/components/Subpage.vue";
 
-class Project_ {
-  constructor(title, description, url, status = undefined) {
+class Subpage_ {
+  constructor(title, url) {
     this.title = title;
-    this.description = description;
     this.url = url;
+  }
+}
+
+class Project_ extends Subpage_ {
+  constructor(title, description, url, status = undefined) {
+    super(title, url);
+    this.description = description;
     if (status) {
       this.status = status;
     }
@@ -14,12 +21,17 @@ class Project_ {
 }
 
 export default {
-  components: {Project, IconTitle},
+  components: {Project, IconTitle, Subpage},
   data() {
     return {
       projects: [
         new Project_("DuScript", "The purpose of creating this language was an experiment to find out their strength. The peculiarity of this language is the modules unlimited by syntax.", "https://github.com/Pinbib/DuScript"),
         new Project_("QLore", "QLore is a tool for testing servers using simple scripts.", "https://github.com/Pinbib/QLore")
+      ],
+      subpages: [
+        new Subpage_("QLore", "/QLore"),
+        new Subpage_("Search", "/search"),
+        new Subpage_("QSub", "/QSub")
       ]
     };
   }
@@ -35,6 +47,10 @@ export default {
     <Project v-for="project in projects" :description="project.description" :title="project.title"
              :url="project.url" class="d-inline-flex"/>
   </div>
+  <div class="subpages">
+    <h1 class="mx-auto text-center">SubPages</h1>
+    <Subpage v-for="subpage in subpages" :title="subpage.title" :url="subpage.url" class="d-inline-flex"/>
+  </div>
 </template>
 <style scoped>
 .projects-list div {
@@ -43,6 +59,16 @@ export default {
 }
 
 .projects-list div:hover {
+  border: solid #388e3c 1px;
+  background-color: rgba(0, 105, 92, 0.02);
+}
+
+.subpages div {
+  margin-left: 2%;
+  margin-top: 1%;
+}
+
+.subpages div:hover {
   border: solid #388e3c 1px;
   background-color: rgba(0, 105, 92, 0.02);
 }
---
+++ src/components/Subpage.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: {
+    title: String,
+    url: String
+  }
+};
+</script>
+
+<template>
+  <div class="card" style="width: 18rem;">
+    <div class="card-body d-inline-flex">
+      <h5 class="card-title">{{ title }}</h5>
+      <a :href="url" class="btn btn-primary" style="margin-left: 50%;">Open</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  background-color: rgba(28, 30, 33, 0.06);
+  border: solid rgba(240, 248, 255, 0.34) 1px;
+  color: aliceblue;
+}
+</style>
